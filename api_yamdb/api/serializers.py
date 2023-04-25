0d1a620a3f5188926e0aaae06d8457da5739ee50
Сергей Лазарь <lazarser@yandex.ru>
2023-04-26 01:10:05 +0300

    add viewset, serializer, url, mixins, permissions for title, genre, category

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
-
-from reviews.models import User
+from django.db import models
+from rest_framework import serializers, validators
+from reviews.models import User, Category, Genre, Title
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -16,3 +16,54 @@ class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    slug = models.fields.SlugField(
+        max_length=50,
+        validators=[
+            validators.UniqueValidator(
+                queryset=Category.objects.all(),
+                message='Slug не уникален.'),
+        ]
+    )
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    slug = models.fields.SlugField(
+        max_length=50,
+        validators=[
+            validators.UniqueValidator(
+                queryset=Genre.objects.all(),
+                message='Slug не уникален.'),
+        ]
+    )
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleViewSerializer(serializers.ModelSerializer):
+    # rating = 
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all(),
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Genre.objects.all(), many=True,
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, SignUpView
+from .views import (
+    UserViewSet, SignUpView, CategoryViewSet, GenreViewSet, TitleViewSet
+)
 
 
 v1_router = DefaultRouter()
 v1_router.register('users', UserViewSet, basename='users')
+v1_router.register('categories', CategoryViewSet)
+v1_router.register('genres', GenreViewSet)
+v1_router.register('titles', TitleViewSet, basename='titles')
+
 
 urlpatterns = [
     path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
