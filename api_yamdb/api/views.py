385394d85e6a8b6bddd24f6473b696367c5cf018
Алексей Мещеряков <luohins89@yandex.ru>
2023-05-02 08:23:14 +0300

    Готовый проект перед первый ревью

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,158 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from django.db import IntegrityError
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.filters import SearchFilter
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.pagination import LimitOffsetPagination
+
+from .serializers import (
+    UserSerializer, SignUpSerializer, TokenSerializer, CategorySerializer,
+    GenreSerializer, TitleViewSerializer, TitleSerializer, ReviewSerializer,
+    CommentSerializer
+)
+from .permissions import IsStaffOrReadOnly, IsAdminOrModeratorOrAuthor
+from .mixins import CDLViewSet
+from .filters import TitleFilter
+from reviews.models import User, Category, Genre, Title, Review, Comment
+from api_yamdb.settings import ADMIN_EMAIL
+
+
+class UserViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+    lookup_field = 'username'
+    filter_backends = (SearchFilter,)
+    search_fields = ('username',)
+
+    @action(methods=['GET', 'PATCH'], detail=False,
+            permission_classes=(IsAuthenticated,))
+    def me(self, request):
+        user = self.request.user
+        if request.method == 'GET':
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = UserSerializer(user, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(role=user.role)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def send_email(user):
+    confirmation_code = default_token_generator.make_token(user)
+    email_subject = 'Код для авторизации'
+    email_text = f'Ваш код для авторизации - {confirmation_code}'
+    admin_email = ADMIN_EMAIL
+    user_email = [user.email]
+    return send_mail(email_subject, email_text, admin_email, user_email)
+
+
+class SignUpView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            user, create = User.objects.get_or_create(
+                username=serializer.validated_data['username'],
+                email=serializer.validated_data['email']
+            )
+        except IntegrityError:
+            return Response('username или email заняты',
+                            status=status.HTTP_400_BAD_REQUEST)
+        send_email(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GetTokenView(APIView):
+    def post(self, request):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data['username']
+        user = get_object_or_404(User, username=username)
+        confirmation_code = serializer.data['confirmation_code']
+        if not default_token_generator.check_token(user, confirmation_code):
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+        token = RefreshToken.for_user(user)
+        return Response(
+            {'token': str(token.access_token)},
+            status=status.HTTP_200_OK
+        )
+
+
+class CategoryViewSet(CDLViewSet):
+    queryset = Category.objects.all().order_by('id')
+    serializer_class = CategorySerializer
+    permission_classes = (IsStaffOrReadOnly,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'slug')
+    lookup_field = 'slug'
+
+
+class GenreViewSet(CDLViewSet):
+    queryset = Genre.objects.all().order_by('id')
+    serializer_class = GenreSerializer
+    permission_classes = (IsStaffOrReadOnly,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'slug')
+    lookup_field = 'slug'
+
+
+class TitleViewSet(ModelViewSet):
+    queryset = Title.objects.all().order_by(
+        'id').annotate(rating=Avg('reviews__score'))
+    permission_classes = (IsStaffOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('retrieve', 'list'):
+            return TitleViewSerializer
+        return TitleSerializer
+
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAdminOrModeratorOrAuthor,)
+    pagination_class = LimitOffsetPagination
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+
+    def get_queryset(self):
+        return Review.objects.filter(title=self.get_title())
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title=self.get_title())
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAdminOrModeratorOrAuthor,)
+    pagination_class = LimitOffsetPagination
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+
+    def get_review(self):
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'),
+                                 title=self.get_title())
+
+    def get_queryset(self):
+        return Comment.objects.filter(review=self.get_review())
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_review())
